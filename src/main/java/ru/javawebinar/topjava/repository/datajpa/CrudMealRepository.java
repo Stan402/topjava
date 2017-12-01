45652baa2dc56671aa077fc6f646ca50dff37c67
Stan402 <konvin@yahoo.com>
2017-12-01 20:34:59 +0300

    topjava: HW05 basic done.

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,31 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Meal m WHERE m.id=?1 AND m.user.id=?2")
+    int delete(int id, int userId);
+
+    Optional<Meal> findByIdAndUserId(Integer id, Integer userId);
+
+    List<Meal> findAllByUserId(Sort sort, Integer userId);
+
+    List<Meal> findAllByUserIdAndDateTimeBetween(Sort sort, Integer userId, LocalDateTime startDate, LocalDateTime endDate);
 }
